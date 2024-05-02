b30f74098bef63d6e9b849fe004453eb80a0ec5d
Dongmin Kim <ddmkim94@gmail.com>
2024-05-02 16:12:56 +0900

    Feat: 게시판 뷰 목업 구현
    - 게시판 리스트 화면 목업을 구현하고, 컨트롤러와 매핑

--- src/main/java/com/fastcampus/projectboard/domain/article/controller/ArticleController.java
+++ src/main/java/com/fastcampus/projectboard/domain/article/controller/ArticleController.java
@@ -2,11 +2,21 @@ package com.fastcampus.projectboard.domain.article.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/articles")
 @RequiredArgsConstructor
 public class ArticleController {
+
+    @GetMapping
+    public String articles(Model model) {
+        model.addAttribute("articles", List.of());
+        return "articles/index";
+    }
 
 }
