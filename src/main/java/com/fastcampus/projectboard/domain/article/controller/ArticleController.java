ec96ff36fb6ae5062ff56221b17af4cfa4fdb5a7
Dongmin Kim <ddmkim94@gmail.com>
2024-05-02 16:40:07 +0900

    Feat: 게시글 상세 뷰 구현
    - 작성한 테스트를 바탕으로 게시글 상세 컨트롤러와 뷰를 구현함.
    - 테스트 통과 확인

--- src/main/java/com/fastcampus/projectboard/domain/article/controller/ArticleController.java
+++ src/main/java/com/fastcampus/projectboard/domain/article/controller/ArticleController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -17,6 +18,14 @@ public class ArticleController {
     public String articles(Model model) {
         model.addAttribute("articles", List.of());
         return "articles/index";
+    }
+
+    @GetMapping("/{articleId}")
+    public String article(@PathVariable Long articleId, Model model) {
+        model.addAttribute("article", "");
+        model.addAttribute("articleComments", List.of());
+
+        return "articles/detail";
     }
 
 }
