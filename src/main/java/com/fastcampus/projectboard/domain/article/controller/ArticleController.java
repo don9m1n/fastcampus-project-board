d668de4e97a79ffb3c8a7e3d8c1c5c3487cbc9e2
Dongmin Kim <ddmkim94@gmail.com>
2024-05-02 15:45:47 +0900

    Test: 게시판 컨트롤러 테스트 정의
    - 게시글 컨트롤러를 생성하고, 관련 테스트 정의

---
+++ src/main/java/com/fastcampus/projectboard/domain/article/controller/ArticleController.java
@@ -0,0 +1,12 @@
+package com.fastcampus.projectboard.domain.article.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/articles")
+@RequiredArgsConstructor
+public class ArticleController {
+
+}
