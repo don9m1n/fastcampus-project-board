6a4eb316ffca4acee11b23aeaad11c205a20fa67
Dongmin Kim <ddmkim94@gmail.com>
2024-05-02 14:04:53 +0900

    Feat: Data Rest 설정
    - 게시글, 댓글 레포지토리를 data rest가 읽을 수 있도록  @RepositoryRestResource 추가

--- src/main/java/com/fastcampus/projectboard/domain/article/repository/ArticleRepository.java
+++ src/main/java/com/fastcampus/projectboard/domain/article/repository/ArticleRepository.java
@@ -2,7 +2,9 @@ package com.fastcampus.projectboard.domain.article.repository;
 
 import com.fastcampus.projectboard.domain.article.model.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface ArticleRepository extends JpaRepository<Article, Long> {
 
 }
