3d108e6de58bc50a6c7fcdde7c14c45a238b9c94
Dongmin Kim <ddmkim94@gmail.com>
2024-05-02 16:39:26 +0900

    Test: 게시글 상세 페이지 테스트 활성화

--- src/test/java/com/fastcampus/projectboard/domain/article/controller/ArticleControllerTest.java
+++ src/test/java/com/fastcampus/projectboard/domain/article/controller/ArticleControllerTest.java
@@ -34,7 +34,6 @@ class ArticleControllerTest {
                 .andExpect(model().attributeExists("articles"));
     }
 
-    @Disabled("테스트 구현중..")
     @DisplayName("[GET] 게시글 상세 페이지 - 정상 호출")
     @Test
     void givenNothing_whenRequestingArticleView_thenReturnArticleView() throws Exception {
