cb10cde3d1b7cb31d11f9067748a05660dc4f157
Dongmin Kim <ddmkim94@gmail.com>
2024-05-07 16:02:14 +0900

    Feat: 게시판에 페이지네이션 적용
    - 게시판에 페이지네이션을 적용하고, 테스트 코드에 페이지네이션 관련 내용을 추가함.

--- src/main/java/com/fastcampus/projectboard/domain/article/controller/ArticleController.java
+++ src/main/java/com/fastcampus/projectboard/domain/article/controller/ArticleController.java
@@ -4,6 +4,7 @@ import com.fastcampus.projectboard.domain.article.dto.response.ArticleResponse;
 import com.fastcampus.projectboard.domain.article.dto.response.ArticleWithCommentsResponse;
 import com.fastcampus.projectboard.domain.article.model.SearchType;
 import com.fastcampus.projectboard.domain.article.service.ArticleService;
+import com.fastcampus.projectboard.domain.article.service.PaginationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -16,12 +17,15 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("/articles")
 @RequiredArgsConstructor
 public class ArticleController {
 
     private final ArticleService articleService;
+    private final PaginationService paginationService;
 
     @GetMapping
     public String articles(
@@ -31,7 +35,10 @@ public class ArticleController {
             Model model) {
 
         Page<ArticleResponse> articles = articleService.searchArticles(searchType, searchValue, pageable).map(ArticleResponse::from);
+        List<Integer> barNumbers = paginationService.getPaginationBarNumbers(pageable.getPageNumber(), articles.getTotalPages());
+
         model.addAttribute("articles", articles);
+        model.addAttribute("paginationBarNumbers", barNumbers);
         model.addAttribute("searchTypes", SearchType.values());
 
         return "articles/index";
