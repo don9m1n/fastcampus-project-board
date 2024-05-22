89f77e3568d6fb1b1016e1f9b7ea8ff303d3d053
Dongmin Kim <ddmkim94@gmail.com>
2024-05-22 15:40:49 +0900

    코드 수정

--- src/main/java/com/fastcampus/projectboard/domain/article/repository/ArticleRepository.java
+++ src/main/java/com/fastcampus/projectboard/domain/article/repository/ArticleRepository.java
@@ -24,7 +24,6 @@ public interface ArticleRepository extends
     Page<Article> findByContentContaining(String content, Pageable pageable);
     Page<Article> findByUserAccount_UserIdContaining(String userId, Pageable pageable);
     Page<Article> findByUserAccount_NicknameContaining(String nickname, Pageable pageable);
-    Page<Article> findByHashtag(String hashtag, Pageable pageable);
 
     void deleteByIdAndUserAccount_UserId(Long articleId, String userId);
 
@@ -34,6 +33,7 @@ public interface ArticleRepository extends
         bindings.including(root.title, root.content, root.hashtags, root.createdAt, root.createdBy);
         bindings.bind(root.title).first(StringExpression::containsIgnoreCase); // like %[value]%
         bindings.bind(root.content).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.hashtags.any().hashtagName).first(StringExpression::containsIgnoreCase);
         bindings.bind(root.createdAt).first(DateTimeExpression::eq);
         bindings.bind(root.createdBy).first(StringExpression::containsIgnoreCase);
     }
