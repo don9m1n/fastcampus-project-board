b4ed245e3dbc1de32fd00716487b812b79bf8b25
Dongmin Kim <ddmkim94@gmail.com>
2024-05-28 13:00:10 +0900

    Feat: 댓글 도메인 업데이트
    - 대댓글 구현을 위해 댓글 도메인에 부모 댓글 id, 자식 댓글 필드 추가

--- src/main/java/com/fastcampus/projectboard/domain/articlecomment/model/ArticleComment.java
+++ src/main/java/com/fastcampus/projectboard/domain/articlecomment/model/ArticleComment.java
@@ -12,7 +12,9 @@ import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
+import java.util.LinkedHashSet;
 import java.util.Objects;
+import java.util.Set;
 
 @Table(indexes = {
         @Index(columnList = "content"),
@@ -37,22 +39,38 @@ public class ArticleComment extends BaseEntity {
     private Article article;
 
     @Setter
+    @Column(updatable = false)
+    private Long parentCommentId; // 부모 댓글 ID
+
+    @ToString.Exclude
+    @OrderBy("createdAt ASC")
+    @OneToMany(mappedBy = "parentCommentId", cascade = CascadeType.ALL)
+    private Set<ArticleComment> childComments = new LinkedHashSet<>(); // 자식 댓글 리스트
+
+    @Setter
     @Column(nullable = false, length = 500)
     private String content;
 
     @Builder
-    private ArticleComment(Article article, UserAccount userAccount, String content) {
+    private ArticleComment(Article article, UserAccount userAccount, Long parentCommentId, String content) {
         this.article = article;
         this.userAccount = userAccount;
+        this.parentCommentId = parentCommentId;
         this.content = content;
     }
 
-    public static ArticleComment of(Article article, UserAccount userAccount, String content) {
+    public static ArticleComment of(Article article, UserAccount userAccount,String content) {
         return ArticleComment.builder()
                 .article(article)
                 .userAccount(userAccount)
+                .parentCommentId(null)
                 .content(content)
                 .build();
+    }
+
+    public void addChildComment(ArticleComment child) {
+        child.setParentCommentId(this.getId());
+        this.getChildComments().add(child);
     }
 
     @Override
